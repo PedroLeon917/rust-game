d01cf0dddaceb9a7ae20b9d05af8d745c1bba32f
PedroLeon917 <pedro.leon@everymundo.com>
2024-05-21 15:06:53 -0400

    Usando num-derive y num-traits

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::fmt::{self};
 
-#[derive(Debug)]
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+
+#[derive(Debug, FromPrimitive)]
 enum Gem {
     Diamond = 1,
     Sapphire,
@@ -9,6 +12,7 @@ enum Gem {
     Onyx,
     Jade    
 }
+
 impl fmt::Display for Gem {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
@@ -21,6 +25,24 @@ impl fmt::Display for Gem {
         }
     }
 }
+
+// Implementando manualmente la funcion from_number 
+// para procesar el vector 
+//
+// impl Gem {
+//     fn from_number(num: u8) -> Option<Gem> {
+//         match num {
+//             1 => Some(Gem::Diamond),
+//             2 => Some(Gem::Sapphire),
+//             3 => Some(Gem::Ruby),
+//             4 => Some(Gem::Topaz),
+//             5 => Some(Gem:: Onyx),
+//             6 => Some(Gem::Jade),
+//             _ => None,
+//         }
+//     }
+// }
+
 fn main() {
     let mut map = [[0; 5]; 5];
     // println!("{map:?}");
@@ -37,7 +59,15 @@ fn main() {
 
     let mut found: Vec<Gem>= Vec::new();
 
-    found.push(Gem::Diamond);
+    // found.push(Gem::Diamond);
 
+    let data = map[1][4];
+
+    // Usando la funcion implementada manualmente.
+    // found.push(Gem::from_number(data).expect("Invalid Number"));
+
+    // Usando al crate num-derive num-traits 
+    found.push(Gem::from_u8(data).expect("Invalid Number"));
+    
     println!("{found:?}")
 }
